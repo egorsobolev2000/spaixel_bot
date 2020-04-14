eb044dd75d5725cf9680f62a63dcf0f81ba3bd44
egorsobolev2000 <web-disain@mail.ua>
2020-04-14 11:24:54 +0300

    Готов обработчик всех событий в боте и сбор полного набора информации

--- post/collect_data.py
+++ post/collect_data.py
@@ -1,35 +1,76 @@
 import os
-import re
 import json
 import datetime
 
 
-def createJSONFile(update):
+def JSONFile(path, data='data', d_or_l='dump'):
+    if d_or_l == 'dump':
+        with open(path, 'w', encoding='utf-8') as outfile:
+            json.dump(data, outfile, indent=3)
+
+    if d_or_l == 'load':
+        with open(path, "r") as read_file:
+            load_data = json.load(read_file)
+        return load_data
+
+
+def writeJSONFile(update):
     d = datetime.datetime.today()
+
+    if update.from_user.username is None:
+        update.from_user.username = f'anonymous{update.from_user.id}'
 
     data = {
         "username": str(update.from_user.username),
         "first_name": str(update.from_user.first_name),
         "last_name": str(update.from_user.last_name),
         "language_code": str(update.from_user.language_code),
+        "messages": [{f'{d.hour}:{d.minute}:{d.second}': update.text}],
+        "actions": [],
     }
 
-    with open(f'./post/logs/{update.from_user.username}${d}.json', 'w', encoding='utf-8') as outfile:
-        json.dump(data, outfile, indent=2)
+    JSONFile(f'./post/logs/{update.from_user.username}${d}.json', data)
 
 
-def message_collector(update):
-    username = update.from_user.username
-    # name.split('$')[0]
+def editJSONFile(username, key, value):
+    d = datetime.datetime.today()
     logs = os.listdir('./post/logs/')
-    if len(logs) == 0:
-        print('Создается файл')
-        createJSONFile(update)
-    else:
-        for f in logs:
-            if f.split('$')[0] == username:
-                print('Файл уже был создан')
-            elif f.split('$')[0] != username:
-                print('Создается файл')
-                createJSONFile(update)
+    for file in logs:
+        if file.split('$')[0] == username:
+            data = JSONFile(f'./post/logs/{file}', d_or_l='load')
+            data.get(key).append({f'{d.hour}:{d.minute}:{d.second}': value})
+            JSONFile(f'./post/logs/{file}', data)
+            break
 
+
+def info_collector(update, m_or_a='m', data='data'):
+
+    def action_controller(un):
+        users = JSONFile('./post/logs/USERS.json', d_or_l='load')
+
+        if un in users.get('users'):
+            print('Файл уже был создан')
+            if m_or_a == 'm':
+                message = update.text
+                editJSONFile(un, 'messages', message)
+            elif m_or_a == 'a':
+                action = data
+                editJSONFile(un, 'actions', action)
+        else:
+            print('Создается файл')
+            writeJSONFile(update)
+            users.get('users').append(username)
+            JSONFile('./post/logs/USERS.json', users)
+
+    if m_or_a == 'a':
+        username = update.username
+        if username is None:
+            username = f'anonymous{update.id}'
+        action_controller(username)
+    elif m_or_a == 'm':
+        username = update.from_user.username
+        if username is None:
+            username = f'anonymous{update.from_user.id}'
+        action_controller(username)
+
+    print('------------------------------------------')
